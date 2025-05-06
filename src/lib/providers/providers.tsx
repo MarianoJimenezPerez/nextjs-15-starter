3d3d4b6bbc83fcf3cb1612cb9f859f46c05ac46b
Mariano Jimenez Perez <marianojimenezperez1@gmail.com>
2025-05-06 20:59:33 +0200

    feat: allow providers

---
+++ src/lib/providers/providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { ReactNode, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        position="left"
+        buttonPosition="bottom-left"
+      />
+
+      {children}
+    </QueryClientProvider>
+  );
+};
+export default Providers;
